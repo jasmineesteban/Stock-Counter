9767f673b83c769d872addb8e9d2a46a96a95237
luigipallarca <jasmineesteban9@gmail.com>
2024-06-20 10:51:41 +0800

    Integrate barcode scanning with ZXing.Net.Mobile

--- MauiApp1/Pages/AddItemPage.xaml.cs
+++ MauiApp1/Pages/AddItemPage.xaml.cs
@@ -1,11 +1,13 @@
-using System;
+ using System;
 using Microsoft.Maui.Controls;
+using ZXing.Net.Maui;
+using ZXing.Mobile;
 
 namespace MauiApp1.Pages;
 
-
 public partial class AddItemPage : ContentPage
 {
+
     public AddItemPage()
     {
         InitializeComponent();
@@ -16,8 +18,25 @@ public partial class AddItemPage : ContentPage
 
     }
 
-    private void Scan_Clicked(object sender, EventArgs e)
+    private async void Scan_Clicked(object sender, EventArgs e)
     {
+        var options = new MobileBarcodeScanningOptions
+        {
+            AutoRotate = true,
+            UseFrontCameraIfAvailable = false
+        };
 
+        var scanner = new MobileBarcodeScanner
+        {
+            TopText = "Hold the camera up to the barcode",
+            BottomText = "Scanning will happen automatically"
+        };
+
+        var result = await scanner.Scan(options);
+        if (result != null)
+        {
+            EntryBarcode.Text = result.Text;
+        }
     }
+
 }
