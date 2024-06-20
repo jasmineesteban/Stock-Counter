87aa3ce1b4d6452bca511809417ebd60db3e735c
luigipallarca <jasmineesteban9@gmail.com>
2024-06-20 18:27:45 +0800

    Text Cleaning

--- MauiApp1/Pages/AddItemPage.xaml.cs
+++ MauiApp1/Pages/AddItemPage.xaml.cs
@@ -1,6 +1,5 @@
- using System;
+using System;
 using Microsoft.Maui.Controls;
-using ZXing.Net.Maui;
 using ZXing.Mobile;
 
 namespace MauiApp1.Pages;
@@ -23,13 +22,16 @@ public partial class AddItemPage : ContentPage
         var options = new MobileBarcodeScanningOptions
         {
             AutoRotate = true,
-            UseFrontCameraIfAvailable = false
+            UseFrontCameraIfAvailable = false,
         };
+
 
         var scanner = new MobileBarcodeScanner
         {
             TopText = "Hold the camera up to the barcode",
-            BottomText = "Scanning will happen automatically"
+            BottomText = "Scanning will happen automatically",
+            CancelButtonText = "Cancel",
+            FlashButtonText = "OK",
         };
 
         var result = await scanner.Scan(options);
@@ -39,4 +41,37 @@ public partial class AddItemPage : ContentPage
         }
     }
 
+    private void OnSearchTextChanged(object sender, TextChangedEventArgs e)
+    {
+
+    }
+
+    private void EntryExpiryDate_TextChanged(object sender, TextChangedEventArgs e)
+    {
+        var entry = (Entry)sender;
+        var text = entry.Text;
+
+        // Remove any non-numeric characters
+        string cleanedText = new string(text.Where(char.IsDigit).ToArray());
+
+        // Handle max length
+        if (cleanedText.Length > 8)
+        {
+            cleanedText = cleanedText.Substring(0, 8);
+        }
+
+        // Format the cleaned text to ####-##-##
+        if (cleanedText.Length > 4 && cleanedText[4] != '-')
+        {
+            cleanedText = cleanedText.Insert(4, "-");
+        }
+        if (cleanedText.Length > 7 && cleanedText[7] != '-')
+        {
+            cleanedText = cleanedText.Insert(7, "-");
+        }
+
+        // Update the Entry text and cursor position
+        entry.Text = cleanedText;
+        entry.CursorPosition = cleanedText.Length;
+    }
 }
