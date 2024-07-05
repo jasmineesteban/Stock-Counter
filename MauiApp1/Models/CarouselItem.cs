e35b05cc0a32c7434cc20abd13efee8bcf006a7a
luigipallarca <jasmineesteban9@gmail.com>
2024-07-05 12:37:18 +0800

    Added Carousel View for startup page

---
+++ MauiApp1/Models/CarouselItem.cs
@@ -0,0 +1,11 @@
+﻿namespace MauiApp1.Models
+{
+    class CarouselItem
+    {
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public string Image { get; set; }
+    }
+}
