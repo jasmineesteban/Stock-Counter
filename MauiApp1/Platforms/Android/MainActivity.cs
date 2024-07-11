32ab214b82fbce7106cfa127d10856c32a001638
luigipallarca <jasmineesteban9@gmail.com>
2024-07-11 10:51:54 +0800

    Add App Icon and splash Screen

--- MauiApp1/Platforms/Android/MainActivity.cs
+++ MauiApp1/Platforms/Android/MainActivity.cs
@@ -5,7 +5,7 @@ using Microsoft.Maui;
 
 namespace MauiApp1
 {
-    [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
+    [Activity( Icon = "@drawable/splash", Theme = "@style/Maui.SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
     public class MainActivity : MauiAppCompatActivity
     {
         
